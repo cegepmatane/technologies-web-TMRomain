61fbd6f38104ff924e9b5545065224c466ab747e
Romain <54318639+TMRomain@users.noreply.github.com>
2019-08-27 14:27:21 -0400

    Ajout du footer et de quel image

--- Document/pagewebPHP/page/dev.php
+++ Document/pagewebPHP/page/dev.php
@@ -2,8 +2,6 @@
 <h1>Devellopeur</h1>
 <br> <br> <br>
 <p>Biographie et histoire de différent devellopeur/Concepteur de jeux videos</p>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <p>Biographie et histoire de différent devellopeur/Concepteur de jeux videos</p>
 
 <?php include('include/Footer.php');?>
--- Document/pagewebPHP/page/include/footer.php
+++ Document/pagewebPHP/page/include/footer.php
@@ -1,2 +1,18 @@
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<div class="stickyl">
+    <div class="FootBar">
+        <div class="floatright">
+            <a href="https://www.facebook.com/"><img src="../images/LogoFace.png" alt="Facebook"
+                    style="width:40px;height:40px;"></a>
+
+            <a href="https://twitter.com/"><img src="../images/LogoTwitter.png" alt="Twitter"
+                    style="width:40px;height:40px;"></a>
+            <a href="https://github.com/"><img src="../images/LogoGit.png" alt="GitHUB"
+                    style="width:40px;height:40px;"></a>
+        </div>
+    </div>
+</div>
 </body>
+
 </html>
