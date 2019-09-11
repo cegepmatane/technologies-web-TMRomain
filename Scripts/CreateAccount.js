5cab3bfcae917a8fde3090a5564cf04dd950639b
Romain <54318639+TMRomain@users.noreply.github.com>
2019-09-11 16:04:19 -0400

    Update Page Crées un Compte

--- Scripts/CreateAccount.js
+++ Scripts/CreateAccount.js
@@ -1,10 +1,72 @@
+var email ;
+function onLoad()
+{
+    if(email == null)
+    {
+        email = document.getElementById("emailCompte");
+    }
+}
 
 function validerCompte(formulaire){
-    var message = formulaire.getElementById("message");
-    var email = formulaire.getElementById("emailCompte");
-    if(email.value.length == 0){
-        message.innerHTML= "Email ne peut pas etre vide";
-        email.style.color ="red";
+    var message = document.getElementById("message");
+    if(email == null)
+    {
+        email = document.getElementById("emailCompte");
     }
-
+    if(email.value == ""){
+       
+        return false;
+    }else{
+        validerCourriel(email);
+    }
+    return false;
+}
+function turnBlue(input){
+    input.style.borderColor = "blue";
+}
+function onFocusEmail(input) {
+    if(email == null)
+    {
+        email = document.getElementById("emailCompte");
+    }
+    if(email.value == "exemple@exemple.exemple")
+    {
+    email.value = "";
+    }
+}
+function outFocusEmail(formulaire)
+{
+    if(email == null)
+    {
+        email = document.getElementById("emailCompte");
+    }
+    if(email.value == "")
+    {
+        email.value = "exemple@exemple.exemple";
+    }
+    
+}
+function unFocus(input){
+    input.style.borderColor = "grey";
+}
+function validerCourriel(email)
+{
+    if(email.value == "" || email.value == "exemple@exemple.exemple")
+    {
+        message.innerHTML = "*L'email ne peut pas être Vide";
+        message.style.color ="red";
+        email.style.borderColor ="red";
+    }else
+    {
+        var emailEntre = email.value;
+        var formatEmail = "^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$";
+        var validateurEmail = new RegExp(formatEmail);
+        var emailFiltre = validateurEmail.exec(emailEntre);    
+        if(emailFiltre == null){
+            message.innerHTML = "*Mauvais format d'email";
+            message.style.color ="red";
+            return false;
+        }
+    }
+ 
 }
