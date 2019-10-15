42dd29fdc1d8ebbd18d0e686e81d101a6fae9a9e
Romain <54318639+TMRomain@users.noreply.github.com>
2019-10-15 16:56:55 -0400

    LIVRAISON_2

--- Scripts/Article.js
+++ Scripts/Article.js
@@ -5,24 +5,28 @@ balise.style.color = "black";
 //La méthode appendChild() va insérer un objet en tant que dernier enfant d’un autre objet.
 document.getElementById("textUpdate").appendChild(balise).innerHTML = "Nombre d'item dans la liste: " + item.length;
 
-let button;
-function LirePlusText(button){
-    let child = button.firstElementChild;
-    let text = button.parentElement.firstElementChild;
-    let parentFirstChild = button.parentElement.firstElementChild;
-    let plus = button.parentElement.firstElementChild.getElementsByClassName("plus");
-    let point = button.parentElement.firstElementChild.getElementsByClassName("point");
-    console.log(plus);
-    if(child.innerHTML == "Lire plus")
-    {
-        plus[0].style.display = "inline";
-        point[0].style.display = "none";
-        text.style.overflowY = "scroll";
-        child.innerHTML = "Lire moin";
-    }else{
-        plus[0].style.display = "none";
-        point[0].style.display = "inline";
-        text.style.overflowY = "hidden";
-        child.innerHTML = "Lire plus";
-    }
-}
+var button;
+button = document.getElementsByClassName("hover-btn");
+Array.from(button).forEach(function(elem) {
+    elem.addEventListener("click", function LirePlusText(elem){
+        console.log(elem);
+        let child = elem.currentTarget.firstElementChild;
+        let text = elem.currentTarget.parentElement.firstElementChild;
+        let parentFirstChild = elem.currentTarget.parentElement.firstElementChild;
+        let plus = elem.currentTarget.parentElement.firstElementChild.getElementsByClassName("plus");
+        let point = elem.currentTarget.parentElement.firstElementChild.getElementsByClassName("point");
+        console.log(plus);
+        if(child.innerHTML == "Lire plus")
+        {
+            plus[0].style.display = "inline";
+            point[0].style.display = "none";
+            text.style.overflowY = "scroll";
+            child.innerHTML = "Lire moin";
+        }else{
+            plus[0].style.display = "none";
+            point[0].style.display = "inline";
+            text.style.overflowY = "hidden";
+            child.innerHTML = "Lire plus";
+        }
+    });
+});
--- Scripts/Footer.js
+++ Scripts/Footer.js
@@ -1,13 +1,51 @@
-let footer = document.getElementById("footer");
-function plusGrand(button){
-    let image = button.firstElementChild;
-    image.style.transition  = 1 + "s";
-    image.style.width  = 30 + "px";
-    image.style.height  = 30 + "px";
-}
-function plusPetit(button){
-    let image = button.firstElementChild;
-    image.style.transition  = 1 + "s";
-    image.style.width  = 20 + "px";
-    image.style.height  = 20 + "px";
-}
+let footer = document.getElementById("FootBar");
+var buttons = document.getElementsByClassName("RSButton");
+
+Array.from(buttons).forEach(function (elem) {
+    elem.addEventListener("mouseover", function () {
+        let image = elem.firstElementChild;
+        image.style.transition = 1 + "s";
+        image.style.width = 30 + "px";
+        image.style.height = 30 + "px";
+    });
+});
+Array.from(buttons).forEach(function (elem) {
+    elem.addEventListener("mouseleave", function() {
+        let image = elem.firstElementChild;
+        image.style.transition = 1 + "s";
+        image.style.width = 20 + "px";
+        image.style.height = 20 + "px";
+    });
+});
+
+
+var elem = document.createElement("img");
+elem.setAttribute("src", "Decoration/LogoFace.png");
+elem.setAttribute("class", "imageRS");
+elem.setAttribute("alt", "Facebook");
+document.getElementById("Facebook").parentElement.appendChild(elem);
+document.getElementById("Facebook").parentElement.removeChild(document.getElementById("Facebook"));
+var elem = document.createElement("img");
+elem.setAttribute("src", "Decoration/LogoTwitter.png");
+elem.setAttribute("class", "imageRS");
+elem.setAttribute("alt", "Twitter");
+document.getElementById("Twitter").parentElement.appendChild(elem);
+document.getElementById("Twitter").parentElement.removeChild(document.getElementById("Twitter"));
+var elem = document.createElement("img");
+elem.setAttribute("src", "Decoration/LogoGit.png");
+elem.setAttribute("class", "imageRS");
+elem.setAttribute("alt", "GitHub");
+document.getElementById("GitHub").parentElement.appendChild(elem);
+document.getElementById("GitHub").parentElement.removeChild(document.getElementById("GitHub"));
+var elem = document.createElement("img");
+elem.setAttribute("src", "Decoration/LogoYou.png");
+elem.setAttribute("class", "imageRS");
+elem.setAttribute("alt", "Youtube");
+document.getElementById("Youtube").parentElement.appendChild(elem);
+document.getElementById("Youtube").parentElement.removeChild(document.getElementById("Youtube"));
+var elem = document.createElement("img");
+elem.setAttribute("src", "Decoration/LogoRe.png");
+elem.setAttribute("class", "imageRS");
+elem.setAttribute("alt", "Reddit");
+document.getElementById("Reddit").parentElement.appendChild(elem);
+document.getElementById("Reddit").parentElement.removeChild(document.getElementById("Reddit"));
